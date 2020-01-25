d2175c252821a296b41b87129303c6033726c7a7
Ricardo Ferreira <thegiantbeast.pt@gmail.com>
2020-01-25 18:51:52 +0000

    Add initial support to fetch data from the API

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import fetch from 'jest-fetch-mock'
 import App from './App';
 
-import table from './data/table-1'
+fetch.enableMocks()
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App table={table} />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const tableMock = {
+  "id": 1001,
+  "state": "open",
+  "game": "holdem",
+  "blinds": {
+    "small": 1,
+    "big": 200
+  },
+  "seats": [],
+  "currentHand": {
+    "id": 12345,
+    "communityCards": [],
+    "players": [],
+    "pots": []
+  }
+}
+
+describe('App', () => {
+  beforeEach(() => {
+    fetch.mockResponse(JSON.stringify(tableMock))
+  })
+
+  it('renders the loading state as the initial state', () => {
+    const wrapper = mount(<App />);
+
+    expect(wrapper.state('isLoading')).toBe(true)
+  });
+
+  it('renders the table state after fetching the API data', async () => {
+    const wrapper = mount(<App />);
+
+    // needs to await otherwise `componentWillMount` won't be executed
+    await wrapper.instance().componentWillMount()
+
+    expect(wrapper.state('table')).toEqual(tableMock)
+    expect(wrapper.state('isLoading')).toBe(false)
+  });
+})
