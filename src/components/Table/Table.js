4603e8540c4620e8c4e90ee383cb0015ddf7c301
Ricardo Ferreira <thegiantbeast.pt@gmail.com>
2020-02-12 22:45:07 +0000

    Change table classes declaration to use classnames package

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { tableShape } from '../../types';
 
 import Seats from '../Seats';
@@ -7,15 +8,23 @@ import Pots from '../Pots';
 
 import './Table.css';
 
-const Table = ({ table }) => (
-  <div className={`Table ${table.game === 'omaha' ? 'Poker-omaha' : 'Poker'}`}>
-    <div>
-      <Seats seats={table.seats} players={table.currentHand.players} />
-      <Cards values={table.currentHand.communityCards} />
-      <Pots pots={table.currentHand.pots} />
+const Table = ({ table }) => {
+  const isOmaha = table.game === 'omaha';
+  const tableClass = classNames('Table', {
+    'Poker-omaha': isOmaha,
+    'Poker': !isOmaha
+  });
+
+  return (
+    <div className={tableClass}>
+      <div>
+        <Seats seats={table.seats} players={table.currentHand.players} />
+        <Cards values={table.currentHand.communityCards} />
+        <Pots pots={table.currentHand.pots} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Table.propTypes = {
   table: tableShape.isRequired,
